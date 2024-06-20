8bc1eff0d0f30c02e6c73243fd688bbeb3860d43
junseooo <pointjunseo@gmail.com>
2024-06-20 18:24:52 +0900

    data fetching and css is done, but there is an error in detail page

--- app/(movies)/movies/[id]/error.tsx
+++ app/(movies)/movies/[id]/error.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-export const metadata = {
-  title: "Error",
-};
-
 export default function ErrorOMG() {
   return <h1>lol something broke...</h1>;
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -2,10 +2,6 @@ import { Suspense } from "react";
 import MovieInfo from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
 
-export const metadata = {
-  title: "Movie Detail",
-};
-
 export default async function MovieDetail({
   params: { id },
 }: {
@@ -13,11 +9,9 @@ export default async function MovieDetail({
 }) {
   return (
     <div>
-      <h3>Movie detail page</h3>
       <Suspense fallback={<h1>Loading movie info</h1>}>
         <MovieInfo id={id} />
       </Suspense>
-      <h3>Videos</h3>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
         <MovieVideos id={id} />
       </Suspense>
