64f658711d49b18c532af20e091adfdeffbad075
junseooo <pointjunseo@gmail.com>
2024-06-20 16:10:45 +0900

    first commit

---
+++ app/(home)/loading.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Home Loading",
+};
+
+export default function Loading() {
+  return <h2>Loading...</h2>;
+}
---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const metadata = {
+  title: "Error",
+};
+
+export default function ErrorOMG() {
+  return <h1>lol something broke...</h1>;
+}
